96bc2a58be075551b93ad858c4137388cead85ec
Vishesh Thakur <visheshsingh16@gmail.com>
2016-11-23 19:35:38 -0500

    Prints multiplication table of 10

---
+++ table_10.java
@@ -0,0 +1,14 @@
+
+public class table_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<11;i++){
+			int j = 10;
+			System.out.println("10 x " + i + " = " + (j*i));
+			
+		}
+
+	}
+
+}
