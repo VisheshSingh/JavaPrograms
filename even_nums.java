09ac826a31c714c8dc43c79be12f43c1a67c9227
Vishesh Thakur <visheshsingh16@gmail.com>
2016-11-23 19:34:35 -0500

    Prints even number between 0 and 100

---
+++ even_nums.java
@@ -0,0 +1,16 @@
+
+public class even_nums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i <=100; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("Even numbers between 0 and 100 are : " + i);
+			}
+		}
+
+	}
+
+}
