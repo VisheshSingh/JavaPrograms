65daa244db6546a2cd2a0c4a5eec36aefd8d0864
Vishesh Thakur <visheshsingh16@gmail.com>
2016-11-23 19:35:10 -0500

    Prints even and odd number between 0 and 25

---
+++ even_odd.java
@@ -0,0 +1,19 @@
+
+public class even_odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < 25; i++){
+			if(i%2==0)
+			{
+				System.out.println("Even numbers: " + i);
+			}
+			else
+			{
+				System.out.println("Odd numbers: " + i);
+			}
+		}
+
+	}
+
+}
