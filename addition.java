1045c43d40aeb50df6959720d3c6c841395ded00
Vishesh Thakur <visheshsingh16@gmail.com>
2016-11-23 19:25:49 -0500

    Addition program

---
+++ addition.java
@@ -0,0 +1,12 @@
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 60, b = 40, c;
+		c = a + b;
+		System.out.println("Result is : " + c);
+
+	}
+
+}
