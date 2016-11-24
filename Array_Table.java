f99379cd870d60e71fe3ef8bc4b9d1899da7ec05
Vishesh Thakur <visheshsingh16@gmail.com>
2016-11-23 19:26:50 -0500

    Elements of array in a table

---
+++ Array_Table.java
@@ -0,0 +1,17 @@
+
+public class Array_Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arrayNums[] = {10,20,30,40,50,60};
+		
+		System.out.println("Index\tValue");
+		for (int counter = 0; counter < arrayNums.length; counter++)
+		{
+			
+			System.out.println(counter + "\t" + arrayNums[counter]);
+		}
+
+	}
+
+}
